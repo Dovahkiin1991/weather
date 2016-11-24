4f48904e499f8ebe74bf9d841a346c2be07c1998
backflipper1991 <sashaulych@gmail.com>
2016-11-24 21:06:05 +0200

    Weather app basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Weather app';
+  desc = 'Application that show weather in current city';
+  city = 'Poltava';
+  api = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
+  link = '&APPID=69c090ab8377610e780d1f3059fd34be&units=metric&cnt=7';
+  today = new Date();
+  days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+  http: Http;
+  collection = [];
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  performSearch(searchTerm: HTMLInputElement): void {
+    var apiLink = this.api + searchTerm.value + this.link;
+
+      this.http.request(apiLink)
+        .subscribe((res: Response) => {
+          this.collection = res.json();
+          console.log(this.collection);
+          // function currentDay(index) {
+            // currentDay = this.days[ this.today.getDay() ];
+            // var currentDay = index;
+            // return currentDay;
+            // console.log(index);
+          // }
+
+        });
+  }
+
 }
